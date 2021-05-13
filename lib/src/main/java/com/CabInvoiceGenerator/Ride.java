dc63f51e1cd73b52b90570dc62e42464e563bb2d
satya-patel <satyaprakashpatel1998@gmail.com>
2021-05-13 16:45:17 +0530

    complete project

---
+++ lib/src/main/java/com/CabInvoiceGenerator/Ride.java
@@ -0,0 +1,13 @@
+package com.CabInvoiceGenerator;
+
+public class Ride {
+	public double distance;
+	public int time;
+	public String rideType;
+
+	public Ride(double distance, int time, String rideType) {
+		this.distance = distance;
+		this.time = time;
+		this.rideType = rideType;
+	}
+}
---
+++ lib/src/test/java/com/CabInvoiceGenerator/InvoiceServiceTest.java
@@ -0,0 +1,50 @@
+package com.CabInvoiceGenerator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+	InvoiceGenerator invoiceGenerator = null;
+
+	@Before
+	public void setUp() throws Exception {
+		invoiceGenerator = new InvoiceGenerator();
+	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldCalculate_ReturnTotalFare() {
+		double distance = 5;
+		int time = 3;
+		String rideType = "normal";
+		double fare = invoiceGenerator.calculateFare(distance, time, rideType);
+		Assert.assertEquals(53, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTime_ShouldCalculate_ReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		String rideType = "premium";
+		double fare = invoiceGenerator.calculateFare(distance, time, rideType);
+		Assert.assertEquals(20, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5, "normal"), new Ride(0.1, 1, "premium") };
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 45.0);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+	}
+
+	@Test
+	public void givenUserId_ShouldReturnTheInvoiceSummary() {
+		int userId = 2;
+		RideRepository rideRepository = new RideRepository();
+		Ride[] rideList = rideRepository.getRideList(userId);
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rideList);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 100.0);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
+	}
+}
